945279c25aca7490a2d5d7aa53b3db93cd0f85bb
accel <faralmtop@gmail.com>
2025-08-06 03:16:08 +0500

    added @ApiProperty to User entity

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -4,18 +4,23 @@ import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
+    @ApiProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiProperty()
     @Column({ length: 500 })
     name: string;
 
+    @ApiProperty()
     @Column("varchar", { length: 320, unique: true })
     email: string;
 
+    @ApiProperty()
     @Column("varchar")
     passwordHash: string;
 
+    @ApiProperty()
     @Column({
         type: "enum",
         enum: Role,
