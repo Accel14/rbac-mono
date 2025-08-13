07002280511f79a469e8470a05d3fdf6b6127d55
accel <faralmtop@gmail.com>
2025-08-14 00:01:07 +0500

    UsersController test now uses Auto mocking

--- backend/src/api/users/users.controller.spec.ts
+++ backend/src/api/users/users.controller.spec.ts
@@ -7,61 +7,90 @@ import { Role } from "@enums/roles.enum";
 import { getRepositoryToken } from "@nestjs/typeorm";
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
+import { ModuleMocker, MockMetadata } from "jest-mock";
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('UsersController', () => {
     let usersController: UsersController;
-    let usersService: UsersService;
-    let userRepository: MockRepository;
 
     type MockRepository<T extends ObjectLiteral = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
-    beforeEach(async () => {
-        const mockUserRepository = {
-            find: jest.fn(),
-            findOne: jest.fn(),
-            save: jest.fn(),
-        };
-        const moduleRef = await Test.createTestingModule({
-            controllers: [UsersController],
-            providers: [
-                UsersService,
-                {
-                    provide: getRepositoryToken(User),
-                    useValue: mockUserRepository,
-                },
-                {
-                    provide: JwtService,
-                    useValue: {
-                        sign: jest.fn(),
-                        verify: jest.fn(),
-                    },
-                },
-                {
-                    provide: ConfigService,
-                    useValue: {
-                        get: jest.fn(),
-                    },
-                },
-            ],
-        }).compile();
+    const mockUsers: User[] = [{
+        id: 1,
+        name: 'Testir',
+        email: 'testmail@google.com',
+        passwordHash: 'hashedPass1',
+        role: Role.Admin,
+    }];
 
-        userRepository = moduleRef.get<MockRepository<User>>(getRepositoryToken(User));
-        usersService = moduleRef.get(UsersService);
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [UsersController]
+        })
+            .useMocker((token) => {
+                if (token === UsersService) {
+                    return { findAll: jest.fn().mockResolvedValue(mockUsers) };
+                }
+
+                if (token === getRepositoryToken(User)) {
+                    return { find: jest.fn().mockResolvedValue(mockUsers) };
+                }
+
+                if (typeof token === 'function') {
+                    const mockMetadata = moduleMocker.getMetadata(token);
+                    if (!mockMetadata) { return {}; }
+                    return moduleMocker.generateFromMetadata(mockMetadata);
+                }
+            })
+            .compile();
+
         usersController = moduleRef.get(UsersController);
     });
 
     describe('findAll', () => {
         it('should return an array of users', async () => {
-            const result = Promise.resolve<User[]>([{
-                id: 1,
-                name: 'Testir',
-                email: 'testmail@google.com',
-                passwordHash: 'hashedPass1',
-                role: Role.Admin,
-            }]);
-            jest.spyOn(usersService, 'findAll').mockImplementation(() => result);
-
-            expect(await usersController.findAll()).toBe(await result);
+            const result = await usersController.findAll();
+            expect(result).toEqual(mockUsers);
         })
     })
+
+
+
+    // beforeEach(async () => {
+    //     const mockUserRepository = {
+    //         find: jest.fn(),
+    //         findOne: jest.fn(),
+    //         save: jest.fn(),
+    //     };
+    //     const moduleRef = await Test.createTestingModule({
+    //         controllers: [UsersController],
+    //         providers: [
+    //             UsersService,
+    //             {
+    //                 provide: getRepositoryToken(User),
+    //                 useValue: mockUserRepository,
+    //             },
+    //             {
+    //                 provide: JwtService,
+    //                 useValue: {
+    //                     sign: jest.fn(),
+    //                     verify: jest.fn(),
+    //                 },
+    //             },
+    //             {
+    //                 provide: ConfigService,
+    //                 useValue: {
+    //                     get: jest.fn(),
+    //                 },
+    //             },
+    //         ],
+    //     }).compile();
+
+    //     userRepository = moduleRef.get<MockRepository<User>>(getRepositoryToken(User));
+    //     usersService = moduleRef.get(UsersService);
+    //     usersController = moduleRef.get(UsersController);
+    // });
+
+
 })
