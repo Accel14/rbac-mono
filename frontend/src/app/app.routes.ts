00aa60b27f4ea701a477356f2b8f475ad8d689f8
accel <faralmtop@gmail.com>
2025-08-14 00:12:04 +0500

    LoginComponent -> Login in app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register';
-import { LoginComponent } from './login/login';
+import { Login } from './login/login';
 import { Profile } from './profile/profile';
 import { UsersList } from './users-list/users-list';
 
